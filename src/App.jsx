db079a324017ae9da898f2a5acc70ee3fa2efcc7
user.email <williaminterlock@gmail.com>
2023-05-31 15:03:35 -0500

    added sign up as well as some error messages

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import BoardPage from './components/BoardPage';
 import Login from './components/Login';
 import HomePage from './components/HomePage';
+import Signup from './components/Signup'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/board" element={<BoardPage />} />
       <Route path="/home" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/" element={<Login />} />
     </Routes>
   );
