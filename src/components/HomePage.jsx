3df9160d030b8d4f62b1fbdbb872065435faf8f4
user.email <williaminterlock@gmail.com>
2023-06-01 10:09:07 -0500

    fix

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import UserBoards from './UserBoards';
 import styled from 'styled-components';
+import { UserContext } from '../userContext';
 
 
 const Page = styled.div`
