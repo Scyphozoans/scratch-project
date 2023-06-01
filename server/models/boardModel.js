513f8697d7af3237324160d06c4e7a7fa187e3fd
jojecameron <jojecameron@gmail.com>
2023-05-31 17:13:27 -0700

    switching to board model from auth

--- server/models/boardModel.js
+++ server/models/boardModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const boardSchema = new Schema({
     boardname: { type: String, required: true },
-    storage: { type: Array, required: true },
+    storage: { type: Array, required: true, default: [[],[],[],[]] },
     users: { type: Object, required: true }
 });
 
